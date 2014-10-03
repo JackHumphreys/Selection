b15139d12a38bdbc9f64dba26ee726c13c64670d
40618 <40618@longroad.ac.uk>
2014-10-03 13:50:52 +0100

    Date convertor finished.

--- date_convertor.py
+++ date_convertor.py
@@ -1,23 +1,72 @@
-#Month Exercise
-date = str(input("Please enter date as DD/MM/YYYY: "))
+#Date Exercise
+date = input("Please enter date as DD/MM/YYYY: ")
+day_input = int(date[:2])
+month_input = int(date[3:5])
+year = int(date[6:10])
 
-date.split("/")
-day,month,year = date.split("/")
+if day_input == 1:
+    day = ("1st")
 
+elif day_input == 2:
+    day = ("2nd")
 
-if day == 1:
-    print("1st")
+elif day_input == 3:
+    day = ("3rd")
 
-elif day == 2:
-    print("2nd")
+elif day_input >= 4 and day_input <=30:
+    day = ("{0}th".format(day_input))
 
-elif day == 3:
-    print("3rd")
-
-elif day == 4:
-    print("{0}th".format(day))
-
-
+else:
+    day = ("N/A")
 
 
 
+
+
+if month_input == 1:
+    month = ("January")
+
+elif month_input == 2:
+    month = ("February")
+
+elif month_input == 3:
+    month = ("March")
+
+elif month_input == 4:
+    month = ("April")
+
+elif month_input == 5:
+    month = ("May")
+
+elif month_input == 6:
+    month = ("June")
+
+elif month_input == 7:
+    month = ("July")
+
+elif month_input == 8:
+    month = ("August")
+
+elif month_input == 9:
+    month = ("September")
+
+elif month_input == 10:
+    month = ("October")
+
+elif month_input == 11:
+    month = ("November")
+    
+elif month_input == 12:
+    month = ("December")
+
+else:
+    month = ("N/A")
+
+
+
+
+
+
+
+
+print(day, month, year)
