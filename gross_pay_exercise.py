fe27d92e8f15191856e264694c944facf1c5982d
40618 <40618@longroad.ac.uk>
2014-10-07 12:36:29 +0100

    Gross pay exercise.

--- gross_pay_exercise.py
+++ gross_pay_exercise.py
@@ -4,14 +4,12 @@
 hours = float(input("Please enter the hours you work in a week: "))
 pay = float(input("Please enter hourly pay: "))
 
-pay_extra = ((hours*pay)+(pay-40)*1.5)
-
-if pay > 40:
-    print("Gross pay is: {0}".format(pay_extra))
-
-elif hours > 60:
+if hours > 60:
     print("ERROR")
 
+elif hours > 40:
+    print("Gross pay is: {0}".format((pay*40)+(hours-40)*(pay*1.5)))
+
 else:
-    print("Gross pay is: {0}".format(hours*pay))
+    print("Gross pay is: {0}".format(pay*hours))
 
