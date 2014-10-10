5beb9eeb949abe7f0a6a21f204506abde32e67db
40618 <40618@longroad.ac.uk>
2014-10-10 14:34:05 +0100

    Attendance.

---
+++ attendance.py
@@ -0,0 +1,5 @@
+att = float(input("Please enter your attendance: "))
+if att > 85:
+          print("Your attendance is {0}%. Keep up the good attendance.".format(att))
+elif att < 85:
+          print("Your attendance is {0}%. You must improve your attendance.".format(att))
